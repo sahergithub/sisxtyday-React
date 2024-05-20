12fcc5a9d91618f31d1cba2387df4b8349db0baa
Saher Fatma <saherfatma012@gmail.com>
2024-05-21 01:36:59 +0530

    ....

---
+++ assignment1react1/a1p2/src/components/ProfileCard.jsx
@@ -0,0 +1,15 @@
+// src/components/ProfileCard.jsx
+
+import React from "react";
+
+function ProfileCard({ name, age, location }) {
+  return (
+    <div>
+      <h3>Name: {"Saher"}</h3>
+      <p>Age: {30}</p>
+      <p>Location: {"Prayagraj"}</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
